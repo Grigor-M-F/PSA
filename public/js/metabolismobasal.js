6152a22a7da0fd010474020f0ad467042297c006
Grigor-M-F <grigor.ferreira@sptech.school>
2024-11-13 15:06:32 -0300

    update nas páginas

--- public/js/metabolismobasal.js
+++ public/js/metabolismobasal.js
@@ -1,23 +1,19 @@
 function calcular() {
     var fem = iptSexo_F.value;
-    var atividade = Number(sltAtividade.value);
+    var biotipo = Number(sltBiotipo.value);
     var altura = Number(iptAltura.value);
     var peso = Number(iptPeso.value);
     var idade = Number(iptIdade.value);
 
-    if (atividade == "#") {
-        erro.innerHTML = `<span style="color: red;"> Selecione um nível de atividade </span>`;
-    }
-
     var calculo = 0;
 
     if (fem == 0) {
-        calculo = atividade * (88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade));
+        calculo = biotipo * (88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade));
     } else {
-        calculo = atividade * (447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade));
+        calculo = biotipo * (447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade));
     }
 
-    resposta.innerHTML = calculo;
+    resposta.innerHTML = `${parseInt(calculo)} kcal`;
 
     console.log(calculo);
 
